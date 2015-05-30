8d23f64fc190975287498efda73b078d5c1b1222
mehr.najafi@gmail.com <mehr.najafi@gmail.com>
2015-05-30 00:09:42 -0400

    initial  commit

---
+++ routes/brands.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+/*
+ * GET userlist.
+ */
+router.get('/brandlist', function(req, res) {
+    var db = req.db;
+    db.collection('brandlist').find().toArray(function (err, items) {
+        res.json(items);
+    });
+});
+
+/*
+ * POST to adduser.
+ */
+router.post('/addbrand', function(req, res) {
+    var db = req.db;
+    db.collection('brandlist').insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deletebrand/:id', function(req, res) {
+    var db = req.db;
+    var userToDelete = req.params.id;
+    db.collection('brandlist').removeById(userToDelete, function(err, result) {
+        res.send((result === 1) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+
+module.exports = router;
