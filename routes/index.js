c21ddb9ddf98d30231a20639c9611682cf2e4dd6
mehr.najafi@gmail.com <mehr.najafi@gmail.com>
2015-06-09 23:08:34 -0400

    dashboards added.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Buy Original Dashboard' });
 });
 
+/* GET brands page. */
+router.get('/brandMgm', function(req, res, next) {
+  res.render('brandMgm', { title: 'Buy Original - Brands Dashboard' });
+});
+
+/* GET categories page. */
+router.get('/categoryMgm', function(req, res, next) {
+  res.render('categoryMgm', { title: 'Buy Original - Categories Dashboard' });
+});
+
+
+
 module.exports = router;
