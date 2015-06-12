235d89f641dccdba863b0f2ae0a13907ffd3ece9
mehr.najafi@gmail.com <mehr.najafi@gmail.com>
2015-06-11 23:54:53 -0400

    no message

--- routes/stores.js
+++ routes/stores.js
@@ -30,12 +30,59 @@ router.get('/storelist/:id', function(req, res) {
  * POST to adduser.
  */
 router.post('/addstore', function(req, res) {
+    console.log('/addstore');
     var db = req.db;
-    db.collection('stores').insert(req.body, function(err, result){
-        res.send(
-            (err === null) ? { msg: '' } : { msg: err }
-        );
-    });
+    
+    // Find CategoryId
+    var _bId = req.body.bId;
+    console.log('_bId:'+_bId);
+  	
+	db.collection('brands').findOne({bId:_bId.toString()},function (err,doc) {
+    if (doc){
+        var _cId = doc.bCategoryId;
+        var _bName = doc.bName;
+        console.log('_cId:'+_cId);
+        console.log('_bName:'+_bName);
+        db.collection('categories').findOne({cId:_cId.toString()},function (err,doc) {
+        if (doc){
+            var _cName=doc.cName;
+        	console.log('_cName:'+_cName);
+        	var newStore = {
+        		'bId': req.body.bId,
+        		'sId':req.body.sId,
+        		'sName':req.body.sName,
+        		'bName':_bName,
+        		'bCategory':_cName,
+        		'bDistributor':req.body.bDistributor,
+        		'sCity':req.body.sCity,
+        		'sAddress':req.body.sAddress,
+        		'sHours':req.body.sHours,
+        		'sAreaCode':req.body.sAreaCode,
+        		'sTel1':req.body.sTel1,
+        		'sTel2':req.body.sTel2,
+        		'sLat':req.body.sLat,
+        		'sLong':req.body.sLong,
+        		'sVerified':req.body.sVerified
+    		}
+        	
+        	db.collection('stores').insert(newStore, function(err, result){
+        	if (err === null) {
+        		console.log('new store doc added');
+        	}
+        	res.send(
+            	(err === null) ? { msg: '' } : { msg: err }
+        		);
+    		});	
+        }
+        else {
+        	console.log('err'+err);
+        	}
+        });
+    } 
+    else {
+        console.log('err'+err);
+    	}
+	});
 });
 
 /*
