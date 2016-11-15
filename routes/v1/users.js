96419a18273c5952dfaa14b8a531c9b613146f43
mehranna@ca.ibm.com <mehr.najafi@gmail.com>
2016-11-14 20:15:19 -0500

    added logs

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -292,7 +292,7 @@ router.post('/business/updateuser', function(req, res) {
         _store.sLat = req.body.buStoreLat,
         _store.sLong = req.body.buStoreLon,
         _store.bLogo = req.body.buBrandLogoName
-        
+        console.log(_store);
         // Delete store record
         return _db.collection('stores').removeAsync({sId:req.body.buStoreId})  	
     })
